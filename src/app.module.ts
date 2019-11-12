09537088f08416fa868686e5da6a183d6f747794
jlzm <changhui1118@foxmail.com>
2019-11-12 22:30:13 +0800

    add-typeORM_env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,20 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './modules/post/post.module';
-
+// const __typeOrm = {
+//   type: 'mysql',
+//   host: 'localhost',
+//   port: 3306,
+//   username: 'nest',
+//   password: 'password',
+//   database: 'nest',
+//   synchronize: true,
+//   logging: true,
+//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+// }
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'nest',
-      password: 'password',
-      database: 'nest',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(),
     PostModule
   ],
   controllers: [AppController],
